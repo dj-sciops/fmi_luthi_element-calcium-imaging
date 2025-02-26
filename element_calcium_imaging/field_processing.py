7d7a397045aca5f4ded2b6e94f71c65a1bd88f17
Thinh Nguyen <thinh@datajoint.com>
2025-02-26 13:51:47 -0600

    fix: major refactor in motion correction & segmentation (optimize for memory)

--- element_calcium_imaging/field_processing.py
+++ element_calcium_imaging/field_processing.py
@@ -67,6 +67,14 @@ class PreProcessing(dj.Computed):
         processing_output_dir: varchar(1000)  #  Output directory of the processed scan relative to root data directory
         """
 
+    class File(dj.Part):
+        definition = """
+        -> master.Field
+        file_name: varchar(255)
+        ---
+        file: filepath@imaging-processed
+        """
+
     @property
     def key_source(self):
         """
@@ -189,6 +197,21 @@ class PreProcessing(dj.Computed):
             }
         )
         self.Field.insert(field_processing_tasks)
+        # Insert result files
+        file_entries = []
+        for field in field_processing_tasks:
+            for f in field["params"]["extra_dj_params"]["image_files"]:
+                f = processed_root_data_dir / f
+                file_entries.append(
+                    {
+                        **key,
+                        "field_idx": field["field_idx"],
+                        "file_name": f.relative_to(prepared_input_dir).as_posix(),
+                        "file": f,
+                    }
+                )
+        logger.info("Inserting %d file entries...", len(file_entries))
+        self.File.insert(file_entries)
 
 
 @schema
@@ -200,6 +223,14 @@ class FieldMotionCorrection(dj.Computed):
     execution_duration: float  # (hour) execution duration
     mc_params: longblob  # parameter set for this run
     """
+
+    class File(dj.Part):
+        definition = """
+        -> master
+        file_name: varchar(255)
+        ---
+        file: filepath@imaging-processed
+        """
 
     def make(self, key):
         execution_time = datetime.now(timezone.utc)
@@ -220,11 +251,9 @@ class FieldMotionCorrection(dj.Computed):
         if acq_software == "PrairieView" and method == "caiman":
             import multiprocessing
             import tifffile
-            from element_interface.run_caiman import _save_mc
             import caiman as cm
-            from caiman.summary_images import local_correlations
-            from caiman.source_extraction.cnmf.cnmf import CNMF
             from caiman.motion_correction import MotionCorrect
+            from caiman.source_extraction.cnmf.cnmf import CNMF
             from caiman.source_extraction.cnmf.params import CNMFParams
 
             file_paths = [
@@ -242,7 +271,7 @@ class FieldMotionCorrection(dj.Computed):
                         )
                     )
 
-            rho = local_correlations(np.dstack(images))
+            rho = cm.summary_images.local_correlations(np.dstack(images))
             half_median_correlation = np.nanmedian(rho) / 2
 
             logger.info("Min correlation set to: %f", half_median_correlation)
@@ -275,26 +304,29 @@ class FieldMotionCorrection(dj.Computed):
                 caiman_temp = os.environ.get("CAIMAN_TEMP")
                 os.environ["CAIMAN_TEMP"] = str(output_dir)
 
-                # use 80% of available cores
-                n_processes = int(np.floor(multiprocessing.cpu_count() * 0.8))
+                # use 60% of available cores (this step should be low memory)
+                n_processes = int(np.floor(multiprocessing.cpu_count() * 0.6))
                 _, dview, n_processes = cm.cluster.setup_cluster(
-                    backend="multiprocessing", n_processes=n_processes
+                    backend="multiprocessing",
+                    n_processes=n_processes,
+                    maxtasksperchild=1,
                 )
                 try:
                     opts = CNMFParams(params_dict=params)
                     cnm = CNMF(n_processes, params=opts, dview=dview)
                     fnames = cnm.params.get("data", "fnames")
-                    logger.info("Starting motion correction (CaImAn)...")
                     mc = MotionCorrect(fnames, dview=cnm.dview, **cnm.params.motion)
-                    mc.motion_correct(save_movie=True)
+                    mc_base_attrs = list(mc.__dict__)
 
-                    fname_mc = (
-                        mc.fname_tot_els
-                        if cnm.params.motion["pw_rigid"]
-                        else mc.fname_tot_rig
-                    )
+                    logger.info("Starting motion correction (CaImAn)...")
+                    # only save F-order mmap if no cropping
+                    mc.motion_correct(save_movie=mc_indices is None)
+                    mc_results = {
+                        k: v for k, v in mc.__dict__.items() if k not in mc_base_attrs
+                    }
+
                     if cnm.params.get("motion", "pw_rigid"):
-                        b0 = np.ceil(
+                        mc_results["b0"] = np.ceil(
                             np.maximum(
                                 np.max(np.abs(mc.x_shifts_els)),
                                 np.max(np.abs(mc.y_shifts_els)),
@@ -309,7 +341,9 @@ class FieldMotionCorrection(dj.Computed):
                         else:
                             cnm.estimates.shifts = [mc.x_shifts_els, mc.y_shifts_els]
                     else:
-                        b0 = np.ceil(np.max(np.abs(mc.shifts_rig))).astype(int)
+                        mc_results["b0"] = np.ceil(
+                            np.max(np.abs(mc.shifts_rig))
+                        ).astype(int)
                         cnm.estimates.shifts = mc.shifts_rig
                 except Exception as e:
                     dview.terminate()
@@ -317,20 +351,9 @@ class FieldMotionCorrection(dj.Computed):
                 else:
                     cm.stop_server(dview=dview)
                     logger.info("Motion correction (CaImAn) complete. Saving results.")
-                    cnmf_mc_output_file = output_dir / (
-                        pathlib.Path(fname_mc[0]).stem + "_cnm_mc.hdf5"
-                    )
+                    base_name = pathlib.Path(fnames[0]).stem
+                    cnmf_mc_output_file = output_dir / (base_name + "_cnm_mc.hdf5")
                     cnm.save(cnmf_mc_output_file.as_posix())
-                    logger.info("Saving motion correction hdf5.")
-                    mc_output_file = output_dir / (
-                        pathlib.Path(fname_mc[0]).stem + "_mc.hdf5"
-                    )
-                    _save_mc(
-                        mc,
-                        mc_output_file.as_posix(),
-                        params["is3D"],
-                        summary_images={},  # skip summary images here
-                    )
                     if caiman_temp is not None:
                         os.environ["CAIMAN_TEMP"] = caiman_temp
                     else:
@@ -339,14 +362,7 @@ class FieldMotionCorrection(dj.Computed):
                     "cnmf_mc_output_file": cnmf_mc_output_file.relative_to(
                         processed_root_data_dir
                     ).as_posix(),
-                    "mc_output_file": mc_output_file.relative_to(
-                        processed_root_data_dir
-                    ).as_posix(),
-                    "fname_mc": [
-                        pathlib.Path(f).relative_to(processed_root_data_dir).as_posix()
-                        for f in fname_mc
-                    ],
-                    "b0": b0,
+                    "mc_results": mc_results,
                 }
                 return extra_dj_params
 
@@ -359,7 +375,6 @@ class FieldMotionCorrection(dj.Computed):
             if mc_indices is not None:
                 # store "indices" in params as tuple instead of the `slice` object
                 params["motion"]["indices"] = mc_indices
-
         else:
             raise NotImplementedError(
                 f"Field motion correction for {acq_software} scans with {method} is not yet supported in this table."
@@ -374,6 +389,17 @@ class FieldMotionCorrection(dj.Computed):
                 "mc_params": params,
             }
         )
+        self.File.insert(
+            [
+                {
+                    **key,
+                    "file_name": f.relative_to(output_dir.parent).as_posix(),
+                    "file": f,
+                }
+                for f in output_dir.rglob("*")
+                if f.is_file()
+            ]
+        )
 
 
 @schema
@@ -385,6 +411,14 @@ class FieldSegmentation(dj.Computed):
     execution_duration: float  # (hour) execution duration
     """
 
+    class File(dj.Part):
+        definition = """
+        -> master
+        file_name: varchar(255)
+        ---
+        file: filepath@imaging-processed
+        """
+
     def make(self, key):
         execution_time = datetime.now(timezone.utc)
         processed_root_data_dir = scan.get_processed_root_data_dir()
@@ -394,9 +428,9 @@ class FieldSegmentation(dj.Computed):
             * FieldMotionCorrection.proj("mc_params")
             & key
         ).fetch1("processing_output_dir", "mc_params")
-        extra_params = params.pop("extra_dj_params", {})
-        output_dir = find_full_path(processed_root_data_dir, output_dir)
+        extra_dj_params = params.pop("extra_dj_params", {})
 
+        output_dir = find_full_path(processed_root_data_dir, output_dir)
         acq_software = (scan.Scan & key).fetch1("acq_software")
         method = (imaging.ProcessingParamSet * imaging.ProcessingTask & key).fetch1(
             "processing_method"
@@ -404,20 +438,16 @@ class FieldSegmentation(dj.Computed):
 
         if acq_software == "PrairieView" and method == "caiman":
             import multiprocessing
-            import h5py
             import caiman as cm
-            from caiman.source_extraction.cnmf.cnmf import load_CNMF
+            from element_interface.run_caiman import _save_mc
+            from caiman.motion_correction import MotionCorrect
+            from caiman.source_extraction.cnmf.cnmf import CNMF, load_CNMF
+            from caiman.source_extraction.cnmf.params import CNMFParams
 
+            mc_results = extra_dj_params.pop("mc_results", {})
             cnmf_mc_output_file = find_full_path(
-                processed_root_data_dir, extra_params["cnmf_mc_output_file"]
+                processed_root_data_dir, extra_dj_params["cnmf_mc_output_file"]
             )
-            mc_output_file = find_full_path(
-                processed_root_data_dir, extra_params["mc_output_file"]
-            )
-            fname_mc = [
-                str(find_full_path(processed_root_data_dir, f))
-                for f in extra_params["fname_mc"]
-            ]
             fnames = [
                 str(find_full_path(processed_root_data_dir, f))
                 for f in params["fnames"]
@@ -427,43 +457,62 @@ class FieldSegmentation(dj.Computed):
             output_dir.mkdir(parents=True, exist_ok=True)
 
             @memoized_result(
-                uniqueness_dict={**params, **extra_params},
+                uniqueness_dict={**params, **extra_dj_params},
                 output_directory=output_dir,
             )
             def _run_segmentation():
-                # use 30% of available cores
-                n_processes = int(np.floor(multiprocessing.cpu_count() * 0.3))
+                # very memory-intensive step, use 5% of available cores only
+                n_processes = int(np.floor(multiprocessing.cpu_count() * 0.05))
                 _, dview, n_processes = cm.cluster.setup_cluster(
-                    backend="multiprocessing", n_processes=n_processes
+                    backend="multiprocessing",
+                    n_processes=n_processes,
+                    maxtasksperchild=1,
                 )
-
-                cnm = load_CNMF(
-                    cnmf_mc_output_file, n_processes=n_processes, dview=dview
-                )
+                cnm = load_CNMF(cnmf_mc_output_file, n_processes=None, dview=None)
                 cnm.params.set("data", {"fnames": fnames})
+
+                mc = MotionCorrect(fnames, dview=cnm.dview, **cnm.params.motion)
+                for k, v in mc_results.items():
+                    setattr(mc, k, v)
 
                 caiman_temp = os.environ.get("CAIMAN_TEMP")
                 os.environ["CAIMAN_TEMP"] = str(output_dir)
                 try:
-                    b0 = 0
-                    base_name = pathlib.Path(fnames[0]).stem + "_memmap_"
-                    data_set_name = cnm.params.get("data", "var_name_hdf5")
-
-                    logger.info("Generating C-order memmap file...")
-                    fname_new = cm.mmapping.save_memmap(
-                        fname_mc,
-                        base_name=base_name,
-                        order="C",
-                        var_name_hdf5=data_set_name,
-                        border_to_0=b0,
+                    base_name = pathlib.Path(fnames[0]).stem
+                    fname_mc = (
+                        mc.fname_tot_els
+                        if cnm.params.motion["pw_rigid"]
+                        else mc.fname_tot_rig
                     )
+                    if all(fname_mc):
+                        # F-order memmap exists (from motion correction)
+                        logger.info("Generating C-order memmap file...")
+                        border_to_0 = (
+                            0 if mc.border_nan == "copy" else mc.border_to_0
+                        )  # trim border against NaNs
+                        fname_new = cm.mmapping.save_memmap(
+                            fname_mc,
+                            base_name=base_name + "_mc",
+                            order="C",
+                            var_name_hdf5=cnm.params.get("data", "var_name_hdf5"),
+                            border_to_0=border_to_0,
+                        )
+                    else:
+                        logger.info("Apply shifts, generating C-order memmap file...")
+                        fname_new = mc.apply_shifts_movie(
+                            fnames,
+                            save_memmap=True,
+                            save_base_name=base_name + "_mc",
+                            order="C",
+                        )
+                        mc.mmap_file = [fname_new]
+
                     Yr, dims, T = cm.mmapping.load_memmap(fname_new)
                     images = np.reshape(Yr.T, [T] + list(dims), order="F")
                     cnm.mmap_file = fname_new
 
                     logger.info("Starting CNMF analysis...")
-                    indices = (slice(None), slice(None))
-                    fit_cnm = cnm.fit(images, indices=indices)
+                    fit_cnm = cnm.fit(images, indices=(slice(None), slice(None)))
 
                     Cn = cm.summary_images.local_correlations(
                         images[:: max(T // 100, 1)], swap_dim=False
@@ -477,12 +526,10 @@ class FieldSegmentation(dj.Computed):
                     cnm2.estimates.evaluate_components(
                         images, cnm2.params, dview=cnm.dview
                     )
-
                     cnm2.estimates.detrend_df_f(quantileMin=8, frames_window=250)
                     cnm2.estimates.Cn = Cn
                     fname_hdf5 = cnm2.mmap_file[:-4] + "hdf5"
                     cnm2.save(fname_hdf5)
-
                 except Exception as e:
                     dview.terminate()
                     raise e
@@ -497,20 +544,14 @@ class FieldSegmentation(dj.Computed):
                         "max_image": np.max(images[:: max(T // 100, 1)], axis=0),
                         "correlation_image": Cn,
                     }
-                    # open mc_output_file, copy the "/motion_correction" over to cnmf_output_file
-                    logger.info("Copy '/motion_correction' to output file...")
-                    with h5py.File(mc_output_file, "r") as h5mc:
-                        with h5py.File(cnmf_output_file, "r+") as h5f:
-                            h5mc.copy("/motion_correction", h5f)
-                            h5g = h5f.get("/motion_correction")
-                            for img_type, img in summary_images.items():
-                                if img_type not in h5g:
-                                    h5g.require_dataset(
-                                        img_type,
-                                        shape=np.shape(img),
-                                        data=img,
-                                        dtype=img.dtype,
-                                    )
+
+                    logger.info("Saving motion correction hdf5.")
+                    _save_mc(
+                        mc,
+                        cnmf_output_file.as_posix(),
+                        params["is3D"],
+                        summary_images=summary_images,
+                    )
 
                     if caiman_temp is not None:
                         os.environ["CAIMAN_TEMP"] = caiman_temp
@@ -530,6 +571,17 @@ class FieldSegmentation(dj.Computed):
                 "execution_time": execution_time,
                 "execution_duration": exec_dur,
             }
+        )
+        self.File.insert(
+            [
+                {
+                    **key,
+                    "file_name": f.relative_to(output_dir.parent).as_posix(),
+                    "file": f,
+                }
+                for f in output_dir.rglob("*")
+                if f.is_file()
+            ]
         )
 
 
@@ -568,6 +620,12 @@ class PostProcessing(dj.Computed):
             imaging.ProcessingTask * imaging.ProcessingParamSet & key
         ).fetch1("processing_output_dir", "processing_method", "params")
 
+        package_version = ""
+        if method == "caiman":
+            import caiman
+
+            package_version = f"CaImAn v{caiman.__version__}"
+
         output_dir = find_full_path(scan.get_processed_root_data_dir(), output_dir)
 
         exec_dur = (datetime.now(timezone.utc) - execution_time).total_seconds() / 3600
@@ -582,7 +640,7 @@ class PostProcessing(dj.Computed):
             {
                 **key,
                 "processing_time": datetime.now(timezone.utc),
-                "package_version": "",
+                "package_version": package_version,
             },
             allow_direct_insert=True,
         )
@@ -595,5 +653,6 @@ class PostProcessing(dj.Computed):
                 }
                 for f in output_dir.rglob("*")
                 if f.is_file()
-            ], allow_direct_insert=True
+            ],
+            allow_direct_insert=True,
         )
